561eca5d026138ceee831feb6cbfa8712df6ac50
Cas Burggraaf <casburggraaf@gmail.com>
2018-02-13 13:04:59 +0100

    Fixed some bugs and added css-linter

---
+++ app/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function () {
+	browserSync.init(['*.html', './static/css/style.css'], {
+		https: true,
+		server: {
+			baseDir: './'
+		}
+	});
+});
+
+gulp.task('default', ['browser-sync'], function () {
+	gulp.watch('./static/css/*.css'); // Watching all scss changes on changes in the background
+});
